3f43e5c1fd87bc5b08b17447351a8735add8664a
Douglas Assad <douglas_ppl1@hotmail.com>
2023-09-09 21:56:17 -0300

    feat: added UserService

---
+++ src/main/java/com/picpaydesafio/domain/transaction/TransactionRepository.java
@@ -0,0 +1,6 @@
+package com.picpaydesafio.domain.transaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository <Transaction, Long>{
+}
